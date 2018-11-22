e6c0ced553586c75881e52a8e5a728c40d56ad9a
Abdirahman Suleman <abdirahmansuleman@Abdirahmans-MacBook-Pro.local>
2018-11-22 17:48:19 -0600

    sign up button using firebase auth added

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import firebase from 'firebase';
+
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html',
 })
 export class SignupPage {
 
+  name: string = "";
+  email: string = "";
+  password: string = ""
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+
+  signUp() {
+    firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then((data) => {  
+      
+      //assigning firebase user interface to new user    
+      let newUser: firebase.User = data.user
+
+      newUser.updateProfile({
+        displayName: newUser.displayName,
+        photoURL: ""
+      }).then(() => {
+        console.log("Profile Updated")
+      
+      }).catch((err) => {
+        console.log(err)
+      })
+
+    }).catch((err) => {
+      console.log(err)
+    })
+  };
 
   goBack() {
     this.navCtrl.pop()
