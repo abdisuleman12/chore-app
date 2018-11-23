25682c1f8ace18468d3c3a8da7e71aaa7266f9ae
Abdirahman Suleman <abdirahmansuleman@Abdirahmans-MacBook-Pro.local>
2018-11-22 21:31:03 -0600

    added logout button on main chore app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { LoginPage } from '../pages/login/login';
 import { SignupPage } from '../pages/signup/signup';
+import { ChoresPage } from '../pages/chores/chores';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = ChoresPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
